a51b4166d7151fa91e13b0039b9b14d2aa06604d
Abhisek Behera <abhisek.bhr1998@gmail.com>
2024-07-24 02:40:30 +0530

    added log to check serviceAcount JSON

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -4,7 +4,7 @@ import * as admin from "firebase-admin";
 // Secure a connection to firebase from backend
 // const serviceAccount = require("../../../permissions.json");
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
-// console.log(serviceAccount);
+console.log(serviceAccount);
 const app = !admin.apps.length
   ? admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
